d24b70961be929a27602686335d4b7359d0d004a
Niwarthana <147362189+H-N-S-Abayawardhana@users.noreply.github.com>
2025-03-08 21:23:49 +0530

    updated the folder structure and installed toast and added Home page

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; 
+
+
+//import main pages
+import Home from "./pages/Home";
+
+//import user pages
+
+//import Admin pages
+
+//import Veterinarian pages
+
+//import Pharmacist pages
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ToastContainer position="top-center" autoClose={3000} />
+        <Routes>
+
+        {/*Add Main Routes Here*/ }
+          <Route path="/" element={<Home />} />
+
+          {/*Add User Routes Here*/ }
+
+
+          {/*Add Admin Routes Here*/ }
+
+
+          {/*Add Veterinarian Routes Here*/ }
+
+
+          {/*Add Pharmacist Routes Here*/ }
+
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
