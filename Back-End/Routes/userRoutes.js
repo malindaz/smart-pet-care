7e17ba1a34e5a6cdf7d9a6e4f328021adfb5ec22
Ishara Umayanga <ish04838@gmail.com>
2025-03-15 15:03:24 +0530

    fixed the userRoute

--- Back-End/Routes/userRoutes.js
+++ Back-End/Routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, getUserProfile } = require('../controllers/userController');
+const { registerUser, loginUser, getUserProfile } = require('../Controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 const multer = require('multer');
 const path = require('path');
