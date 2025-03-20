01e614a82b28087e841260274025e949e48ef231
Niwarthana <147362189+H-N-S-Abayawardhana@users.noreply.github.com>
2025-03-20 17:32:57 +0530

    Fixed few issues and added admin dashboard

--- Front-End/src/components/Admin/AdminNavBar.js
+++ Front-End/src/components/Admin/AdminNavBar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import '../css/Admin/AdminNavBar.css';
+import '../../css/Admin/AdminNavBar.css';
 
 const AdminNavBar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
